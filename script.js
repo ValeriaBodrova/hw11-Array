ef488449599a5df04c38d9edf1244bc2906dbf55
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-06-27 21:58:34 +0200

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, item){
+    let index = array.indexOf(item);
+
+    return array.splice(index, 1);
+}
+
+removeElement(array, 5);
+console.log(array);
+// Результат: [1, 2, 3, 4, 6, 7]
+
+
